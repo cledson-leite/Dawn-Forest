b8210ddc7a824fe3fc41ce763745286090e2936f
cledson leite <40147313+cledson-leite@users.noreply.github.com>
2024-12-28 16:09:57 -0300

    feature de ataque, defeça e abaixar ao personagem

--- scripts/player/Player.gd
+++ scripts/player/Player.gd
@@ -6,7 +6,12 @@ onready var sprite: Sprite = $textura
 
 var velocity: Vector2 = Vector2.ZERO
 var jumpCount: int = 0
+
 var landing: bool = false
+var attacking: bool = false
+var defending: bool = false
+var crouching: bool = false
+var can_tracking_input: bool = true
 
 export(int) var speed
 export(int) var jumpPower
@@ -15,6 +20,7 @@ export(int) var gravity
 func _physics_process(delta: float):
   horizontalMovimentEnvironment()
   verticalMovimentEnvironment()
+  actionEnvironment()
   gravityPlayer(delta)
 
   velocity = move_and_slide(velocity, Vector2.UP)
@@ -22,15 +28,51 @@ func _physics_process(delta: float):
 
 func horizontalMovimentEnvironment() -> void:
   var direction: float = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+  if can_tracking_input == false or attacking:
+    velocity.x = 0
+    return
+  
   velocity.x = direction * speed
+
 
 func verticalMovimentEnvironment() -> void:
   if is_on_floor():
     jumpCount = 0
 
-  if Input.is_action_just_pressed("ui_select") and jumpCount < 2:
+  var jump_condition: bool = can_tracking_input and  not attacking
+  if Input.is_action_just_pressed("ui_select") and jumpCount < 2  and jump_condition:
     jumpCount += 1
     velocity.y = -jumpPower
+
+func actionEnvironment() -> void:
+  attack()
+  defense()
+  crouch()
+
+func attack() -> void:
+  var attack_condition: bool = not attacking and not defending and not crouching
+  if Input.is_action_just_pressed("attack") and attack_condition and is_on_floor():
+    attacking = true
+    sprite.normal_attack = true
+  
+
+func defense() -> void:
+  if Input.is_action_pressed("defense") and not crouching and is_on_floor():
+    defending = true
+    can_tracking_input = false
+  elif not crouching:
+    defending = false
+    can_tracking_input = true
+    sprite.shield_off = true
+
+func crouch() -> void:
+  if Input.is_action_pressed("crouch") and not defending and is_on_floor():
+    crouching = true
+    can_tracking_input = false
+  elif not defending:
+    crouching = false
+    can_tracking_input = true
+    sprite.crouch_off = true
 
 func gravityPlayer(delta: float) -> void:
   velocity.y += gravity * delta
--- scripts/player/PlayerTextura.gd
+++ scripts/player/PlayerTextura.gd
@@ -8,9 +8,16 @@ export(NodePath) var playerPath;
 onready var animation: AnimationPlayer = get_node(animationPath)
 onready var player: Player = get_node(playerPath)
 
+var suffix: String = "Right"
+var normal_attack: bool = false
+var shield_off: bool = false
+var crouch_off: bool = false
+
 func animated(direction: Vector2) -> void:
 	flipTexture(direction)
-	if direction.y != 0:
+	if player.attacking or player.defending or player.crouching:
+		actionBihavior()
+	elif direction.y != 0:
 		verticalBehavior(direction)
 	elif player.landing == true:
 		animation.play("Landing")
@@ -21,14 +28,26 @@ func animated(direction: Vector2) -> void:
 func flipTexture(direction: Vector2) -> void:
 	if direction.x < 0:
 		set_flip_h(true)
+		suffix = "Left"
 	elif direction.x > 0:
 		set_flip_h(false)
+		suffix = "Right"
 
 func horizontalBehavior(direction: Vector2) -> void:
 	if direction.x != 0:
 		animation.play("Run")
 	else:
 		animation.play("Idle")
+
+func actionBihavior() -> void:
+	if player.attacking and normal_attack:
+		animation.play("Attack" + suffix)
+	elif player.defending and shield_off:
+		animation.play("Shield")
+		shield_off = false
+	elif player.crouching and crouch_off:
+		animation.play("Crouch")
+		crouch_off = false
 
 func verticalBehavior(direction: Vector2) -> void:
 	if direction.y < 0:
@@ -44,3 +63,11 @@ func _on_animation_finished(anim_name: String) -> void:
 		"Landing":
 			player.landing = false
 			player.set_physics_process(true)
+
+		"AttackLeft":
+			player.attacking = false
+			normal_attack = false
+
+		"AttackRight":
+			player.attacking = false
+			normal_attack = false
